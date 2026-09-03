e1811d9b4266d1ef8e74ebe5822f8f3cb78ddb88
admin-fixer-2 <dev@local-landlord.com>
2026-09-03 21:50:27 +0800

    fix(miniapp): point dev API to self-hosted server

--- packages/miniapp/config/dev.js
+++ packages/miniapp/config/dev.js
@@ -1,11 +1,11 @@
 module.exports = {
   env: { NODE_ENV: '"development"' },
   defineConstants: {
-    API_BASE: '"http://127.0.0.1:3100/api"',
+    API_BASE: '"https://api.wulianzhijia.cn/api"',
     APP_USE_CLOUD: 'false',
     APP_CLOUD_ENV_ID: '""',
     APP_CLOUD_SVC: '""',
-    APP_H5_BASE: '"http://localhost:3000/h5"',
+    APP_H5_BASE: '"https://api.wulianzhijia.cn/h5"',
   },
   mini: {},
   h5: {},
